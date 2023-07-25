4583f8a87dd96e1a8d65536159541b727dfd5cfc
jackest-gong <jackest.gong@lic.co.nz>
2023-07-25 16:51:34 +1200

    init

---
+++ maui/NewScreen.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace maui;
+
+public partial class NewScreen : ContentPage
+{
+	public NewScreen()
+	{
+		InitializeComponent();
+	}
+    private async void OnButtonClick(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+}
